e8bdcb43379f2e70315a787abaac1fa9aa80109a
Bernabe Davila <bernabeus123@gmail.com>
2021-05-26 20:54:27 -0500

    Creacion de las tarjetas

--- flutter_prueba/lib/pages/card_page.dart
+++ flutter_prueba/lib/pages/card_page.dart
@@ -5,53 +5,41 @@ class CardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cards'),
-      ),
+      appBar: AppBar(),
       body: GridView.count(
         crossAxisCount: 2,
-        padding: EdgeInsets.all(10.0),
         children: <Widget>[
-          _card(),
+          cardR(),
+          cardR(),
+          cardR(),
+          cardR(),
         ],
       ),
     );
   }
+}
 
-  Widget _card() {
-    final card = Container(
-      child: Column(
-        children: <Widget>[
-          FadeInImage(
-            image: NetworkImage(
-                'https://upload.wikimedia.org/wikipedia/commons/9/91/Oahu_Landscape.jpg'),
-            placeholder: AssetImage('assets/jar-loading.gif'),
-            fadeInDuration: Duration(milliseconds: 200),
-            height: 200.0,
-            width: 200.0,
-            fit: BoxFit.cover,
-          ),
-          Container(
-              padding: EdgeInsets.all(10.0), child: Text('No se que poner'))
-        ],
-      ),
-    );
+class cardR extends StatelessWidget {
+  const cardR({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30.0),
-          color: Colors.white,
-          boxShadow: <BoxShadow>[
-            BoxShadow(
-                color: Colors.black26,
-                blurRadius: 10.0,
-                spreadRadius: 2.0,
-                offset: Offset(2.0, 10.0))
-          ]),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(30.0),
-        child: card,
-      ),
-    );
+        width: 300.0,
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Center(
+              child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Image(
+                image: NetworkImage(
+                    'https://rickandmortyapi.com/api/character/avatar/193.jpeg'),
+              ),
+              Container(
+                  padding: EdgeInsets.all(10.0), child: Text('No se que poner'))
+            ],
+          )),
+        ));
   }
 }
